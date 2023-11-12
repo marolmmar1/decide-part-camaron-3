328b21bc7196e48ffe5baf991336ad17a96e8d7d
adrrf <adrromflo@alum.us.es>
2023-11-12 13:10:49 +0100

    feat(postproc): added postproc model
    se ha creado el modelo de postproc. se han añadido los campos voting_id para identificar a que votacion pertenece, question_id para identificar a que pregunta se le hace el post procesado (en el caso en el que haya varias en un futuro) y el campo type para definir que tipo de post procesado se realiza. estos tres campos serán únicos por cada postprocesado realizado.

--- decide/postproc/models.py
+++ decide/postproc/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+class PostProcessing(models.Model()):
+    class Type(models.TextChoices):
+        DEFAULT = "DEF", _("DEFAULT")
+        BORDA = "BOR", _("BORDA")
+        DHONDT = "DHO", _("DHONDT")
+        PARITY = "PAR", _("PARITY")
+    voting_id = models.PositiveIntegerField()
+    question_id = models.PositiveIntegerField()
+    type = models.CharField(max_length=3, choices=Type.choices, default=Type.DEFAULT)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    results = models.JSONField(blank=True, null=True)
+    
+    class Meta:
+        unique_together = (('voting_id', 'question_id', 'type'),)
+        
+    def __str__(self):
+        return f"{self.type} - v{self.voting_id} - q{self.question_id} - \n \t results: {self.results}"
