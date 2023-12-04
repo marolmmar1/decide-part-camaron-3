f29a728d49edfa8bfc83b44ec6abb7d9e8107565
Francisco-David <fd.vilaplana@gmail.com>
2023-12-04 11:50:41 +0100

    feat(controlPanel): added dashboard and dashboard content
    se ha creado el apartado para el dashboard en el modulo store, se han añadido contado de votos por votacion y porcentaje de votos con respecto al censo asociado a esta. Además se añade una barra de progreso para visualizar este último dato.

--- decide/store/admin.py
+++ decide/store/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Vote
+from census.models import Census   
 
-admin.site.register(Vote)
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('voting_id', 'voter_id', 'voted')
+    search_fields = ('voting_id', 'voter_id')
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        votes = Vote.objects.values('voting_id').annotate(total_votes=Count('voting_id'))
+        for vote in votes:
+            census = Census.objects.filter(voting_id=vote['voting_id']).values('voter_id').distinct().count()
+            vote['percentage'] = (vote['total_votes'] / census) * 100 if census else 0
+        extra_context['votes'] = votes
+        return super().changelist_view(request, extra_context=extra_context)
+
+admin.site.register(Vote, VoteAdmin)
