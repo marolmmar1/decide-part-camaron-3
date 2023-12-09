7b2b91d80b6cc8cc6fe5b0523706ce4cf2c76f03
Francisco-David <fd.vilaplana@gmail.com>
2023-12-09 18:34:35 +0100

    style(controlPanel): deleted unusued prints for coding
    Se han eliminado prints innecesarios usados durante el desarrollo de la implementación de datos en tiempo real y bloques de código comentados

--- decide/store/consumers.py
+++ decide/store/consumers.py
@@ -23,7 +23,6 @@ class VoteConsumer(AsyncWebsocketConsumer):
             await event_handler(content)
 
     async def connect(self):
-        print("Connection established")
         # Cuando el WebSocket se conecta, añádelo al grupo 'votes'.
         await self.channel_layer.group_add('votes', self.channel_name)
         await self.accept()
@@ -31,35 +30,8 @@ class VoteConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         pass
 
-    # async def receive(self, text_data):
-    #     text_data_json = json.loads(text_data)
-    #     print(text_data_json)
-    #     vote_id = text_data_json['vote_id']
-    #     print(vote_id)
-
-    #     # Busca la votación en la base de datos
-    #     voting = Voting.objects.get(id=vote_id)
-
-    #     # Cuenta los votos para esta votación
-    #     vote_count = Vote.objects.filter(voting_id=vote_id).count()
-
-    #     # Cuenta el número total de votantes elegibles
-    #     total_voters = Census.objects.filter(voting_id=vote_id).count()
-
-    #     # Calcula el porcentaje de votos
-    #     vote_percentage = (vote_count / total_voters) * 100 if total_voters else 0
-
-    #     await self.send(text_data=json.dumps({
-    #         'message': 'Vote received',
-    #         'vote_id': vote_id,  # Envía el ID de la votación al cliente
-    #         'vote_count': vote_count,  # Envía el recuento de votos al cliente
-    #         'vote_percentage': vote_percentage,  # Envía el porcentaje de votos al cliente
-    #     }))
-
     async def vote_added(self, event):
-        print("vote_added method called")
         vote_id = event['vote_id']
-        print(vote_id)
 
         # Busca la votación en la base de datos
         voting = await sync_to_async(Voting.objects.get)(id=vote_id)
