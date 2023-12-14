af0555d03ea7d1a76757740f163794c00d12f53b
adrrf <adrromflo@alum.us.es>
2023-12-14 10:54:54 +0100

    fix(store): fixed admin store
    resuelve #66. se ha corregido el bug de la vista de admin en el apartado store añadiendo el app_name en el urls de store

--- decide/store/urls.py
+++ decide/store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'store'
+
 urlpatterns = [
     path('', views.StoreView.as_view(), name='store'),
     path('vote/create_backup/', views.create_backup, name='vote_create_backup'),
