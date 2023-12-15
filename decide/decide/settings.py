abdd53116c49d55448639e66bcebe3aa407c801d
Moffinguer <Moffinguer@users.noreply.github.com>
2023-12-15 12:30:06 +0000

    style: Autoformat

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -168,7 +168,7 @@ ALLOWED_VERSIONS = ["v1", "v2"]
 DEFAULT_VERSION = "v1"
 
 try:
-    from  local_settings import *
+    from local_settings import *
 except ImportError:
     print("local_settings.py not found")
 
