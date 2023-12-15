84070e74d683c3fc967c64d4a4b18a659ae80a99
carzarrei <carzarrei@alum.us.es>
2023-12-15 18:01:08 +0100

    feat(voteHistory): Cambiada la funcionalidad tras el mal entendimiento de los votos

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
-from store import views
 
 schema_view = get_swagger_view(title="Decide API")
 
@@ -25,8 +24,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("doc/", schema_view),
     path("gateway/", include("gateway.urls")),
-    path("history/", views.VoteHistoryView.as_view()),
 ]
 
 for module in settings.MODULES:
-    urlpatterns += [path("{}/".format(module), include("{}.urls".format(module)))]
+    urlpatterns += [path("{}/".format(module),
+                         include("{}.urls".format(module)))]
--- decide/store/urls.py
+++ decide/store/urls.py
@@ -12,11 +12,13 @@ urlpatterns = [
         name="vote_create_backup_with_name",
     ),
     path("vote/restore_backup/", views.restore_backup, name="vote_restore_backup"),
-    path("vote/list_backup/", views.list_backups, name="vote_restore_backup_list"),
+    path("vote/list_backup/", views.list_backups,
+         name="vote_restore_backup_list"),
     path("vote/delete_backups/", views.delete_backups, name="delete_backups"),
     path(
         "vote/delete_backup/<str:selected_backup>/",
         views.delete_selected_backup,
         name="delete_backup",
     ),
+    path("voteHistory/", views.VoteHistoryView.as_view()),
 ]
