85e6285b44c88f7755b18e84654cb306323b19b7
Francisco-David <fd.vilaplana@gmail.com>
2023-12-15 16:19:57 +0100

    fix(controlPanel): solved codacy errors

--- decide/store/views.py
+++ decide/store/views.py
@@ -10,7 +10,7 @@ from django.conf import settings
 
 import subprocess
 from django.contrib import messages
-from django.http import HttpResponseBadRequest, HttpResponseBadRequest, HttpResponseRedirect
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Vote
@@ -128,12 +128,6 @@ def list_backups(request):
 
     return render(request, 'list_backups.html', {'backup_files': backup_files})
 
-def list_backups(request):
-    backup_dir = settings.DATABASE_BACKUP_DIR
-    backup_files = list(os.listdir(backup_dir))
-
-    return render(request, 'list_backups.html', {'backup_files': backup_files})
-
 
 def restore_backup(request):
     if request.method == 'POST':
@@ -161,29 +155,6 @@ def delete_selected_backup(request, selected_backup):
         selected_backup = request.POST.get('selected_backup', None)
         if selected_backup:
             backup_path = os.path.join(settings.DATABASE_BACKUP_DIR, selected_backup)
-            if os.path.exists(backup_path) and backup_path.endswith(".psql.bin") and not ".." in backup_path:
-                os.remove(backup_path)
-                messages.success(request, f'Backup "{selected_backup}" deleted successfully.')
-            else:
-                messages.error(request, 'Error deleting backup: Backup file not found')
-                return HttpResponseBadRequest(f'Error deleting backup: Backup file not found: {selected_backup}')     
-        else:
-            messages.error(request, 'No backup selected for deletion.')
-        return HttpResponseRedirect(reverse('store:delete_backups'))
-    else:
-        return render(request, 'confirm_delete.html', {'selected_backup': selected_backup})
-
-def delete_backups(request):
-    backup_files = list(os.listdir(settings.DATABASE_BACKUP_DIR))
-    return render(request, 'delete_backups.html', {'backups': backup_files})
-
-
-def delete_selected_backup(request, selected_backup):
-    if request.method == 'POST':
-        selected_backup = request.POST.get('selected_backup', None)
-        if selected_backup:
-            backup_path = os.path.join(
-                settings.DATABASE_BACKUP_DIR, selected_backup)
             if os.path.exists(backup_path) and backup_path.endswith(".psql.bin") and not ".." in backup_path:
                 os.remove(backup_path)
                 messages.success(
