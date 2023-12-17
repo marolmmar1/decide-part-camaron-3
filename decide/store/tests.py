8ffca58e27aa4c0c2697278e31ab557a02c362ae
Francisco-David <fd.vilaplana@gmail.com>
2023-12-17 05:32:04 +0100

    fix(controlPanel): deleted unused imports

--- decide/store/tests.py
+++ decide/store/tests.py
@@ -35,8 +35,6 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 import time
 import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.support.ui import Select
