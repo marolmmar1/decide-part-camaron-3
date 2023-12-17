38c935ac03bdd9ae900f5c2b563dd6a227469597
[[marolmmar1]] <[[marolmmar1@alum.us.es]]>
2023-12-17 22:22:55 +0100

    test: unit tests of export census and export votes dict

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,4 +1,5 @@
 from base.tests import BaseTestCase
+import itertools
 import json
 from base import mods
 from voting.models import Voting, Question, QuestionOption
@@ -11,11 +12,14 @@ from mixnet.mixcrypt import MixCrypt, ElGamal
 from base.tests import BaseTestCase
 
 from voting.models import Voting, Question
+from .views import build_census_map, build_vote_map
 
 
 # Create your tests here.
 
-"""
+
+
+
 class visualizerTest(BaseTestCase):
     def setUp(self):
         super().setUp()
@@ -132,10 +136,6 @@ class visualizerTest(BaseTestCase):
             len(json.loads(response.context_data["census"])["voters"]), 100
         )
         #self.assertEqual(len(json.loads(response.context_data["voting"])["tally"]), 9)
- 
- 
-"""
-
 
 class visualizerExportCensus(BaseTestCase):
     def setUp(self):
@@ -207,18 +207,29 @@ class visualizerExportCensus(BaseTestCase):
                 mods.post("store", json=data)
         return clear
 
-    def test_not_open(self):
+    
+    
+    def test_export_census_dict(self):
         v = self.create_voting()
         self.create_voters(v)
 
         v.create_pubkey()
+        v.start_date = timezone.now()
         v.save()
 
-        response = self.client.get("/visualizer/" + str(v.id) + "/")
-
-        self.assertIsNone(json.loads(response.context_data["voting"])["tally"])
-
-    def test_visualizer_open_data(self):
+        _ = self.store_votes(v)
+        r = mods.get("voting", params={"id": v.id})
+        a = json.loads(json.dumps(r[0]))
+        c = mods.get("census", params={"voting_id": v.id})
+        i =0
+        res = build_census_map(c.get("voters"))
+        while i<len(res.get('Id')):
+            self.assertEquals(User.objects.get(pk=int(res.get('Id')[i])).username, str(res.get('Name')[i]))
+            i = i+1
+        
+        
+    
+    def test_export_votes_dict(self):
         v = self.create_voting()
         self.create_voters(v)
 
@@ -228,27 +239,13 @@ class visualizerExportCensus(BaseTestCase):
 
         _ = self.store_votes(v)
 
-        response = self.client.get("/visualizer/" + str(v.id) + "/")
-        self.assertEqual(
-            len(json.loads(response.context_data["census"])["voters"]), 100
-        )
-        self.assertIsNone(json.loads(response.context_data["voting"])["tally"])
-
-    def test_visualizer_closed_data(self):
-        v = self.create_voting()
-        self.create_voters(v)
-
-        v.create_pubkey()
-        v.start_date = timezone.now()
-        v.save()
-
-        _ = self.store_votes(v)
-
-        self.login()  # set token
-        v.tally_votes(self.token)
-
-        response = self.client.get("/visualizer/" + str(v.id) + "/")
-
-        self.assertEqual(
-            len(json.loads(response.context_data["census"])["voters"]), 100
-        )
+        r = mods.get("voting", params={"id": v.id})
+        a = json.loads(json.dumps(r[0]))
+        res =build_vote_map(a)
+        i=0
+        for option in res.get('question 1').get('option'):
+            opt =a.get('questions')[0].get("options")[i].get('option')
+            self.assertEquals(option, opt)
+            i=i+1
+            
+    
