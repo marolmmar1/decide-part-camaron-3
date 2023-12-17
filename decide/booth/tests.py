8c5325b303bb37b4d56d45e9129dd8f3116a8e4e
IsmaelHerrera2000 <IsmaelHerrera2000@users.noreply.github.com>
2023-12-17 16:54:29 +0000

    style: Autoformat

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -9,15 +9,11 @@ from django.contrib.auth.models import User
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from django.test import TestCase
 from base.tests import BaseTestCase
 from voting.models import Voting, Question, QuestionOption
 from mixnet.models import Auth
 from django.conf import settings
 from census.models import Census
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 
 # Create your tests here.
@@ -236,38 +232,41 @@ class BoothTestCase(StaticLiveServerTestCase):
             self.driver.find_element(By.CSS_SELECTOR, "h1").text == "Not Found"
         )
 
+
 class BoothTestCaseSelenium(StaticLiveServerTestCase):
-   
     def setUp(self):
         self.client = APIClient()
-        self.base = BaseTestCase()  
+        self.base = BaseTestCase()
         self.base.setUp()
-        self.vars={}
-        
+        self.vars = {}
+
         options = webdriver.ChromeOptions()
         options.headless = True
         self.driver = webdriver.Chrome(options=options)
 
-        #Create questions
-        q = Question(desc='test question')
+        # Create questions
+        q = Question(desc="test question")
         q.save()
-        
+
         for i in range(5):
-            opt = QuestionOption(question=q, number=i, option='option {}'.format(i+1))
+            opt = QuestionOption(question=q, number=i, option="option {}".format(i + 1))
             opt.save()
-        #Create voting and add the questions to it
-        v = Voting(name='test voting')
+        # Create voting and add the questions to it
+        v = Voting(name="test voting")
         v.save()
         v.questions.add(q)
-        au = Auth(name='test auth', url=self.live_server_url, me=True)
+        au = Auth(name="test auth", url=self.live_server_url, me=True)
         au.save()
-        a, _ = Auth.objects.get_or_create(url=settings.BASEURL, defaults={'me': True, 'url':self.live_server_url, 'name': 'me'})
+        a, _ = Auth.objects.get_or_create(
+            url=settings.BASEURL,
+            defaults={"me": True, "url": self.live_server_url, "name": "me"},
+        )
         a.save()
         v.auths.add(au)
 
-        #Create user
-        u = User(username='voter1')
-        u.set_password('decidepass123')
+        # Create user
+        u = User(username="voter1")
+        u.set_password("decidepass123")
         u.is_staff = True
         u.is_superuser = True
         u.save()
@@ -275,7 +274,7 @@ class BoothTestCaseSelenium(StaticLiveServerTestCase):
     def test_booth_login_fail(self):
         voting = Voting.objects.get(name="test voting")
         user = User.objects.get(username="voter1")
-        
+
         census = Census(voting_id=voting.pk, voter_id=user.pk)
         census.save()
 
@@ -283,7 +282,7 @@ class BoothTestCaseSelenium(StaticLiveServerTestCase):
         self.driver.find_element(By.ID, "id_username").send_keys("voter1")
         self.driver.find_element(By.ID, "id_password").send_keys("decidepass123")
         self.driver.find_element(By.CSS_SELECTOR, "form input[type='submit']").click()
-        self.driver.get(self.live_server_url+'/admin/voting/voting/')
+        self.driver.get(self.live_server_url + "/admin/voting/voting/")
         self.driver.find_element(By.NAME, "_selected_action").click()
         dropdown = self.driver.find_element(By.NAME, "action")
         dropdown.find_element(By.XPATH, "//option[. = 'Start']").click()
@@ -297,20 +296,19 @@ class BoothTestCaseSelenium(StaticLiveServerTestCase):
         actions = ActionChains(self.driver)
         actions.move_to_element(element).release().perform()
         self.driver.find_element(By.NAME, "index").click()
-        
 
         self.driver.get(f'{self.live_server_url+"/booth/"+voting.id.__str__()}')
         element = self.driver.find_element(By.CSS_SELECTOR, ".btn-secondary")
-        
-        navbar_toggler = self.driver.find_element(By.CLASS_NAME, 'navbar-toggler')
+
+        navbar_toggler = self.driver.find_element(By.CLASS_NAME, "navbar-toggler")
         navbar_toggler.click()
         actions = ActionChains(self.driver)
         actions.move_to_element(element).perform()
         self.driver.find_element(By.CSS_SELECTOR, ".btn-secondary").click()
-        
+
         element = self.driver.find_element(By.CSS_SELECTOR, "body")
         actions = ActionChains(self.driver)
-        
+
         time.sleep(1)
         self.driver.find_element(By.ID, "username").click()
         time.sleep(1)
@@ -318,7 +316,10 @@ class BoothTestCaseSelenium(StaticLiveServerTestCase):
         self.driver.find_element(By.ID, "password").send_keys("decidepass1234")
         self.driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
         time.sleep(5)
-        self.assertTrue(self.driver.find_element(By.CSS_SELECTOR, ".alert").text == "Error: Bad Request")
+        self.assertTrue(
+            self.driver.find_element(By.CSS_SELECTOR, ".alert").text
+            == "Error: Bad Request"
+        )
 
     def tearDown(self):
         super().tearDown()
