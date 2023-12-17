e5a9e13ce49a6d6380db5fcd8a6e4b312b03840e
Francisco-David <Francisco-David@users.noreply.github.com>
2023-12-17 04:21:00 +0000

    style: Autoformat

--- decide/store/tests.py
+++ decide/store/tests.py
@@ -29,18 +29,17 @@ from django.db import transaction
 from rest_framework.authtoken.models import Token
 
 from selenium import webdriver
-from voting.models import QuestionOption
-from mixnet.models import Auth
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+<<<<<<< HEAD
 import time
 import json
 from selenium.webdriver.common.action_chains import ActionChains
+=======
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+>>>>>>> dc15d7c (style: Autoformat)
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.support.ui import Select
 
