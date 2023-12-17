009b69f3b774cad850a5141611d0ad2c3b4d1501
carzarrei <carzarrei@users.noreply.github.com>
2023-12-17 17:12:20 +0000

    style: Autoformat

--- decide/postproc/migrations/0001_initial.py
+++ decide/postproc/migrations/0001_initial.py
@@ -5,27 +5,60 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
-        ('voting', '0001_initial'),
+        ("voting", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='PostProcessing',
+            name="PostProcessing",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('type', models.CharField(choices=[('NON', 'NONE'), ('DHO', 'DHONDT'), ('PAR', 'SAINT'), ('DRO', 'DROOP')], default='NON', max_length=3)),
-                ('start_date', models.DateTimeField(blank=True, null=True)),
-                ('end_date', models.DateTimeField(blank=True, null=True)),
-                ('results', models.JSONField(blank=True, null=True)),
-                ('question', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='voting.question')),
-                ('voting', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='voting.voting')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "type",
+                    models.CharField(
+                        choices=[
+                            ("NON", "NONE"),
+                            ("DHO", "DHONDT"),
+                            ("PAR", "SAINT"),
+                            ("DRO", "DROOP"),
+                        ],
+                        default="NON",
+                        max_length=3,
+                    ),
+                ),
+                ("start_date", models.DateTimeField(blank=True, null=True)),
+                ("end_date", models.DateTimeField(blank=True, null=True)),
+                ("results", models.JSONField(blank=True, null=True)),
+                (
+                    "question",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="voting.question",
+                    ),
+                ),
+                (
+                    "voting",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="voting.voting",
+                    ),
+                ),
             ],
             options={
-                'unique_together': {('voting_id', 'question_id', 'type')},
+                "unique_together": {("voting_id", "question_id", "type")},
             },
         ),
     ]
